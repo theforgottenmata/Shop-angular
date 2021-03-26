f62d7de61f81a8e0b85873d53c9e8a7369f2938e
Martin Slábek <martin.slabek@educanet.cz>
2021-03-26 17:04:15 +0100

    First

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/product';
+
+const apiUrl = 'http://localhost:3000/products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(apiUrl);
+  }
+}
